77395be19c4a0174f747aee8f9917cc5ca0bc857
raxitzora <raxitzora@gmail.com>
2026-06-18 01:05:17 +0530

    Backend Ready To Deploy

--- src/app.js
+++ src/app.js
@@ -11,7 +11,9 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173",
+      "https://madhavart.vercel.app/",
+    ],
     credentials: true,
   })
 );
