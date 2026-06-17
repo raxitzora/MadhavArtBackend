975badc17749933ade39882f05f764c6e3b64c7e
raxitzora <raxitzora@gmail.com>
2026-06-18 01:42:27 +0530

    TEST 2

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(
   cors({
     origin: ["http://localhost:5173",
-      "https://madhavart.vercel.app/admin/login",
+      "https://madhavart.vercel.app",
     ],
     credentials: true,
   })
